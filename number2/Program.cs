196cbfd88da676f65d0ac836d7bc5e1d673d711e
«Vlad» <>
2022-08-26 02:44:05 +0300

    Number 2

---
+++ number2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
