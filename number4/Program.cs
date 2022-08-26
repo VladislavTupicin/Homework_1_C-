05572684b8b86f454578fd0d50ea9607d173949c
«Vlad» <>
2022-08-26 03:30:21 +0300

    Number 4

---
+++ number4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= number; i++)
+
+if (i % 2 == 0)
+{
+    Console. Write(i + ", ");
+}
